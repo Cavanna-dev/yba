5403bf91183f102a7468f4b6fe26e692a18fbd8b
KeurbyCandy <cavannachristophe@gmail.com>
2016-10-10 01:39:47 +0200

    prod

--- View/default.php
+++ View/default.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="/yba/public/css/main.css" />
+        <link rel="stylesheet" type="text/css" href="/public/css/main.css" />
         <title>You Better Answer !</title>
     </head>
     <body>
@@ -13,6 +13,6 @@
         </div>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
         <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="/yba/public/js/main.js"></script>
+        <script type="text/javascript" src="/public/js/main.js"></script>
     </body>
 </html>
